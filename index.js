012a95346c375ba23910953d4f129c29fa4b9ab1
SolderingKnowledge <imaikol@yahoo.com>
2019-09-01 21:57:57 -0700

    nodeJS  debugger

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
 function arraySubset(arr, sub) {
-    if(sub.length > arr.length) {
+    if (sub.length > arr.length) {
         return false;
     }
 
     const arrCount = {};
 
-    for(let i = 0; i < arr.length; i++) {
+    for (let i = 0; i < arr.length; i++) {
         const item = arr[i];
-        if(arrCount[item]) {
+        if (arrCount[item]) {
             arrCount[item]++;
         } else {
             arrCount[item] = 1;
         }
     }
 
-    for(let i = 0; i < sub.length; i++) {
+    for (let i = 0; i < sub.length; i++) {
         const currentItem = sub[i];
-        if(!arrCount[currentItem]) {//it makes sure there is no items left
+        if (!arrCount[currentItem]) { //it makes sure there is no items left
             return false;
         }
 
         arrCount[currentItem]--;
-        if(arrCount[currentItem] === 0) {
+        if (arrCount[currentItem] === 0) {
             delete arrCount[currentItem];
         }
     }
